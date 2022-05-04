4569d87e0dc74e3fa6db7fab1f5bc1e2a7d38dfa
BLD\FriedRice <anthonyweido@gmail.com>
2022-05-04 11:09:12 -0700

    Node List Swapping

--- Algos/LeetCode/24_swap-nodes-pair/algo.js
+++ Algos/LeetCode/24_swap-nodes-pair/algo.js
@@ -10,5 +10,37 @@
  * @return {ListNode}
  */
  var swapPairs = function(head) {
+    console.log(head);
+    //So I need to swap the two nodes...
+    //The first one points to the third one
+    //the second one points to the first one
+    //then we need to keep track of the first one
+    //get the 3rd relative to it
+    //then swap relative second and third
     
+    const swap = (first, second) =>{
+      
+        let third = first.next?.next
+        
+        second.next = first;
+        first.next = third;
+        return first;
+    }
+    
+
+    if(head === null || head.next == null) return head; 
+    let runner = head;
+    let newHead = head.next;
+        
+    runner = swap(runner, runner.next)
+    
+    while(runner.next !== null && runner.next?.next !== null)
+    {
+        let next = runner.next.next
+        let current = runner;
+        runner = swap(runner.next, runner.next.next)
+        current.next = next;
+    }
+    
+    return newHead;
 };
